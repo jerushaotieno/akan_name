e2078f92175db9f38b6d419cc9454059508df012
jerushaotieno <jerushaotienocoding@gmail.com>
2021-12-12 15:01:04 +0300

    add an array for reproducing user birthday

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
-var akanName = function(year, month, day, gender) {
+var akanName = function(year, month, date, gender) {
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var date = [new Date(year, month, date)];
+    if (gender)
 }
