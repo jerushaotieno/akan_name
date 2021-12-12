84a830b2e73610fb646188868f48f724567f90fb
jerushaotieno <jerushaotienocoding@gmail.com>
2021-12-12 21:17:22 +0300

    modify README file and add a LICENSE to the project

--- scripts.js
+++ scripts.js
@@ -1,10 +1,29 @@
-var akanName = function(year, month, date, gender) {
-    var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+// Backend JS code
+
+var akanName = function(y, m, d, g) {
+    var maleNames = ["Kwasi", "Kudwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-    var date = [new Date(year, month, date)];
-    if (gender == "Male") {
-        return date && maleNames[date.getDay()];
+    var d = new Date(y, --m, d);
+    if (g === "Male") {
+        return d && maleNames[d.getDay()];
     } else {
-        return date && femaleNames[date.getDay()];
+        return d && femaleNames[d.getDay()];
     }
 }
+
+// Frontend JS code
+
+$(document).ready(function() {
+    $("form#form").submit(function(event) {
+        event.preventDefault();
+        var y = parseInt($("#year_of_birth").val());
+        var m = parseInt($("#birth_month").val());
+        var d = parseInt($("#birth_date").val());
+        var g = $("input:radio[name=gender]:checked").val();
+        var result = akanName(y, m, d, g);
+        alert("My Akan name is: " + result);
+
+//refresh page
+        document.getElementById("form").reset();
+    });
+});
