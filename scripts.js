aa7c3e50f7f69df71c1bfdcf6abddb41a80333ff
jerushaotieno <jerushaotienocoding@gmail.com>
2021-12-12 14:44:53 +0300

    add female names array to Akan Name function

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 var akanName = function(year, month, day, gender) {
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var femaleNames = ["Akosua", "Adwoa", ];
+    var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 }
